7dff2c8e63b4453b75f285b63f78ce62029fbb78
Debojyoti Ghosh <debojyoti.ghosh@proptiger.com>
2018-04-21 13:04:16 +0530

    Viewport meta tag fixed.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <title>Xentropoly</title>
 
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-width=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <link href="css/base.css" rel="stylesheet">
         <link href="css/board.css" rel="stylesheet">
